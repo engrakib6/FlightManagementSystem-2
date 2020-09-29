2adc7cb822ed4331cca389f55c49b02e8ac3d5c0
Vineel Mathew <vineelrudrapati9@gmail.com>
2020-09-29 13:45:34 +0530

    airport and usermodule files added

---
+++ user-module/src/main/java/com/dxctraining/usermodule/data_transfer/CreateUserRequest.java
@@ -0,0 +1,49 @@
+package com.dxctraining.usermodule.data_transfer;
+
+public class CreateUserRequest {
+	private String userType;
+	private String userName;
+	private String password;
+	private Long userPhone;
+	private String email;
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(Long userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
